de0ad0c35159959c1bb9478c04cf871da4738c38
MD-AZMAL <azmalmd12@gmail.com>
2019-06-16 22:33:28 +0530

    added script to tear down network

--- artifacts/runContainers.sh
+++ artifacts/runContainers.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+
+function dkcl(){
+        CONTAINER_IDS=$(docker ps -aq)
+	echo
+        if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" = " " ]; then
+                echo "========== No containers available for deletion =========="
+        else
+                docker rm -f $CONTAINER_IDS
+        fi
+	echo
+}
+
+function dkrm(){
+        DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
+	echo
+        if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" = " " ]; then
+		echo "========== No images available for deletion ==========="
+        else
+                docker rmi -f $DOCKER_IMAGE_IDS
+        fi
+	echo
+}
+
+
 echo
 echo " ____    _____      _      ____    _____ "
 echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
@@ -24,6 +48,15 @@ echo
 echo "#################################################################"
 echo "##### Executing Docker command for starting the network #########"
 echo "#################################################################"
+
+set -x
+docker-compose -f docker-compose.yaml down
+res=$?
+set+x
+
+dkcl
+dkrm
+rm -rf ./fabric-client-kv-org*
 
 set -x
 docker-compose -f docker-compose.yaml up
