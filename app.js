ad88eba9631d8ab31f1dc459d0698d5e2a3c3836
MD-AZMAL <azmalmd12@gmail.com>
2019-06-18 13:33:08 +0530

    REST API

---
+++ app.js
@@ -0,0 +1,86 @@
+'use strict';
+var log4js = require('log4js');
+var logger = log4js.getLogger('SampleWebApp');
+var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+var util = require('util');
+var app = express();
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var bearerToken = require('express-bearer-token');
+var cors = require('cors');
+
+require('./config.js');
+var hfc = require('fabric-client');
+
+var helper = require('./app/helper.js');
+var createChannel = require('./app/create-channel.js');
+var join = require('./app/join-channel.js');
+var updateAnchorPeers = require('./app/update-anchor-peers.js');
+var install = require('./app/install-chaincode.js');
+var instantiate = require('./app/instantiate-chaincode.js');
+var invoke = require('./app/invoke-transaction.js');
+var query = require('./app/query.js');
+var host = process.env.HOST || hfc.getConfigSetting('host');
+var port = process.env.PORT || hfc.getConfigSetting('port');
+
+
+// configs
+app.options('*', cors());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.set('secret', 'thisismysecret');
+app.use(expressJWT({
+	secret: 'thisismysecret'
+}).unless({
+	path: ['/users']
+}));
+app.use(bearerToken());
+
+app.use(function(req, res, next) {
+	logger.debug(' ------>>>>>> new request for %s',req.originalUrl);
+	if (req.originalUrl.indexOf('/users') >= 0) {
+		return next();
+	}
+
+	var token = req.token;
+	jwt.verify(token, app.get('secret'), function(err, decoded) {
+		if (err) {
+			res.send({
+				success: false,
+				message: 'Failed to authenticate token. Make sure to include the ' +
+					'token returned from /users call in the authorization header ' +
+					' as a Bearer token'
+			});
+			return;
+		} else {
+			req.username = decoded.username;
+			req.orgname = decoded.orgName;
+			logger.debug(util.format('Decoded from JWT token: username - %s, orgname - %s', decoded.username, decoded.orgName));
+			return next();
+		}
+	});
+});
+
+// server
+
+var server = http.createServer(app).listen(port, function() {});
+logger.info('****************** SERVER STARTED ************************');
+logger.info('***************  http://%s:%s  ******************',host,port);
+server.timeout = 340000;
+
+function getErrorMessage(field) {
+	var response = {
+		success: false,
+		message: field + ' field is missing or Invalid in the request'
+	};
+	return response;
+}
+
+// end points
