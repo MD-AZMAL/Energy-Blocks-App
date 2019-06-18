6fc250c51ff9513951ba4ec64e733db32c6b9955
MD-AZMAL <azmalmd12@gmail.com>
2019-06-18 13:47:51 +0530

    modified imports

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ require('./config.js');
 var hfc = require('fabric-client');
 
 var helper = require('./app/helper.js');
-var createChannel = require('./app/create-channel.js');
+//var createChannel = require('./app/create-channel.js');
 var join = require('./app/join-channel.js');
-var updateAnchorPeers = require('./app/update-anchor-peers.js');
+//var updateAnchorPeers = require('./app/update-anchor-peers.js');
 var install = require('./app/install-chaincode.js');
-var instantiate = require('./app/instantiate-chaincode.js');
-var invoke = require('./app/invoke-transaction.js');
-var query = require('./app/query.js');
+//var instantiate = require('./app/instantiate-chaincode.js');
+//var invoke = require('./app/invoke-transaction.js');
+//var query = require('./app/query.js');
 var host = process.env.HOST || hfc.getConfigSetting('host');
 var port = process.env.PORT || hfc.getConfigSetting('port');
 
@@ -84,3 +84,35 @@ function getErrorMessage(field) {
 }
 
 // end points
+// Register and enroll user
+app.post('/users', async function(req, res) {
+	var username = req.body.username;
+	var orgName = req.body.orgName;
+	logger.debug('End point : /users');
+	logger.debug('User name : ' + username);
+	logger.debug('Org name  : ' + orgName);
+	if (!username) {
+		res.json(getErrorMessage('\'username\''));
+		return;
+	}
+	if (!orgName) {
+		res.json(getErrorMessage('\'orgName\''));
+		return;
+	}
+	var token = jwt.sign({
+		exp: Math.floor(Date.now() / 1000) + parseInt(hfc.getConfigSetting('jwt_expiretime')),
+		username: username,
+		orgName: orgName
+	}, app.get('secret'));
+	let response = await helper.getRegisteredUser(username, orgName, true);
+	logger.debug('-- returned from registering the username %s for organization %s',username,orgName);
+	if (response && typeof response !== 'string') {
+		logger.debug('Successfully registered the username %s for organization %s',username,orgName);
+		response.token = token;
+		res.json(response);
+	} else {
+		logger.debug('Failed to register the username %s for organization %s with::%s',username,orgName,response);
+		res.json({success: false, message: response});
+	}
+
+});
