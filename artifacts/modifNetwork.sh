69a8089d8daa51dd037d72c1fadb7283721da7f2
MD-AZMAL <azmalmd12@gmail.com>
2019-06-19 13:04:45 +0530

    updated

--- artifacts/modifNetwork.sh
+++ artifacts/modifNetwork.sh
@@ -21,3 +21,23 @@ sed -i "s/{ca3}/$(cd channel/crypto-config/peerOrganizations/org3.example.com/us
 res=$?
 set +x
 echo "Modified ca3 credentials"
+
+cp docker-compose-bkp.yaml docker-compose.yaml
+
+set -x
+sed -i "s/{ca1}/$(cd channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/ && ls *_sk)/g" docker-compose.yaml
+res=$?
+set +x
+echo "Modified ca1 credentials"
+
+set -x
+sed -i "s/{ca2}/$(cd channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/keystore/ && ls *_sk)/g" docker-compose.yaml
+res=$?
+set +x
+echo "Modified ca2 credentials"
+
+set -x
+sed -i "s/{ca3}/$(cd channel/crypto-config/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/keystore/ && ls *_sk)/g" docker-compose.yaml
+res=$?
+set +x
+echo "Modified ca3 credentials"
