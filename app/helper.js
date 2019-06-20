4fb69b43f5d37657cf6993d5395166ee8d390746
MD-AZMAL <azmalmd12@gmail.com>
2019-06-20 16:14:58 +0530

    fixed missing affiliation

--- app/helper.js
+++ app/helper.js
@@ -34,28 +34,57 @@ async function getClientForOrg (userorg, username) {
 	return client;
 }
 
-var getRegisteredUser = async function(username, userOrg, isJson) {
+var getRegisteredUser = async function (username, userOrg, isJson) {
 	try {
 		var client = await getClientForOrg(userOrg);
 		logger.debug('Successfully initialized the credential stores');
+		// client can now act as an agent for organization Org1
+		// first check to see if the user is already enrolled
 		var user = await client.getUserContext(username, true);
 		if (user && user.isEnrolled()) {
 			logger.info('Successfully loaded member from persistence');
+			var response = {
+				success: false,
+			};
+			return response;
 		} else {
-			logger.info('User %s was not enrolled, so we will need an admin user object to register',username);
+			// user was not enrolled, so we will need an admin user object to register
+			logger.info('User %s was not enrolled, so we will need an admin user object to register', username);
 			var admins = hfc.getConfigSetting('admins');
-			let adminUserObj = await client.setUserContext({username: admins[0].username, password: admins[0].secret});
+			let adminUserObj = await client.setUserContext({ username: admins[0].username, password: admins[0].secret });
+
+			//newly added
+			if (adminUserObj.getAffiliation() != userOrg.toLowerCase()) {
+				logger.info('Admin affiliation not registered. Registering now.');
+				adminUserObj.setAffiliation(userOrg.toLowerCase());
+				adminUserObj.setRoles(['peer', 'orderer', 'client', 'user']);
+				adminUserObj = await client.setUserContext(adminUserObj);
+			}
+
+			logger.info('Admin User: %s', adminUserObj);
+			let affiliation = userOrg.toLowerCase() + '.department1';
+
 			let caClient = client.getCertificateAuthority();
+
+			//check if org exists
+			const affiliationService = caClient.newAffiliationService();
+			const registeredAffiliations = await affiliationService.getAll(adminUserObj);
+			if (!registeredAffiliations.result.affiliations.some(x => x.name == userOrg.toLowerCase())) {
+				logger.info('Register the new affiliation: %s ', affiliation);
+				await affiliationService.create({ name: affiliation, force: true }, adminUserObj);
+			}
 
 			let secret = await caClient.register({
 				enrollmentID: username,
-				//affiliation: userOrg.toLowerCase() + '.department1'
+				affiliation: affiliation
 			}, adminUserObj);
-			logger.debug('Successfully got the secret for user %s',username);
-			user = await client.setUserContext({username:username, password:secret});
-			logger.debug('Successfully enrolled username %s  and setUserContext on the client object', username);
+			logger.debug('Successfully got the secret for user %s — %s', username);
+
+			user = await client.setUserContext({ username: username, password: secret });
+			logger.debug('Successfully enrolled username %s and setUserContext on the client object', username);
+
 		}
-		if(user && user.isEnrolled) {
+		if (user && user.isEnrolled) {
 			if (isJson && isJson === true) {
 				var response = {
 					success: true,
@@ -67,9 +96,14 @@ var getRegisteredUser = async function(username, userOrg, isJson) {
 		} else {
 			throw new Error('User was not enrolled ');
 		}
-	} catch(error) {
-		logger.error('Failed to get registered user: %s with error: %s', username, error.toString());
-		return 'failed '+error.toString();
+	} catch (error) {
+		let message = 'Failed to register the username ' + username + ' for organization ' + userOrg + ' with error: ' + error.toString();
+		logger.error(message);
+		var response = {
+			success: false,
+			message: message,
+		};
+		return response;
 	}
 
 };
