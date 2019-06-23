8737b0008809a4e0f164890d09b30222f2e5ca52
shubhsherl <shubham.singh094@yahoo.com>
2019-06-23 07:03:25 +0530

    add test for chaincode

--- testAPI.sh
+++ testAPI.sh
@@ -190,6 +190,151 @@ curl -s -X POST \
 echo
 echo
 
+echo "POST instantiate chaincode on Org1"
+echo
+curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d "{
+	\"chaincodeName\":\"mycc\",
+	\"chaincodeVersion\":\"v0\",
+	\"chaincodeType\": \"$LANGUAGE\",
+	\"args\":[]
+}"
+echo
+echo
+
+echo "POST init freq chaincode on peers of Org1 and Org2"
+echo
+VALUES=$(curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes/mycc \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d "{
+  \"peers\": [\"peer0.org1.example.com\",\"peer0.org2.example.com\"],
+  \"fcn\":\"initFreq\",
+  \"args\":[\"121212\",\"121224\",\"50.02\"]
+}")
+echo $VALUES
+# Assign previous invoke transaction id  to TRX_ID
+MESSAGE=$(echo $VALUES | jq -r ".message")
+TRX_ID=${MESSAGE#*ID:}
+echo
+
+echo "GET query freq chaincode on peer1 of Org1"
+echo
+curl -s -X GET \
+  "http://localhost:4000/channels/mychannel/chaincodes/mycc?peer=peer0.org1.example.com&fcn=readFreq&args=%5B%22121212%22%5D" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo
+echo
+
+echo "POST init unit chaincode on peers of Org1 and Org2"
+echo
+VALUES=$(curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes/mycc \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d "{
+  \"peers\": [\"peer0.org1.example.com\",\"peer0.org2.example.com\"],
+  \"fcn\":\"initUnit\",
+  \"args\":[\"Org1\",\"Org2\",\"121212\",\"121224\",\"0.3\"]
+}")
+echo $VALUES
+# Assign previous invoke transaction id  to TRX_ID
+MESSAGE=$(echo $VALUES | jq -r ".message")
+TRX_ID=${MESSAGE#*ID:}
+echo
+
+echo "GET query unit Block by blockNumber"
+echo
+BLOCK_INFO=$(curl -s -X GET \
+  "http://localhost:4000/channels/mychannel/blocks/2?peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json")
+echo $BLOCK_INFO
+# Assign previous block hash to HASH
+HASH=$(echo $BLOCK_INFO | jq -r ".header.previous_hash")
+echo
+
+echo "POST init bill chaincode on peers of Org1 and Org2"
+echo
+VALUES=$(curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes/mycc \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d "{
+  \"peers\": [\"peer0.org1.example.com\",\"peer0.org2.example.com\"],
+  \"fcn\":\"initBill\",
+  \"args\":[\"Org1\",\"Org2\",\"121212\",\"121224\"]
+}")
+echo $VALUES
+# Assign previous invoke transaction id  to TRX_ID
+MESSAGE=$(echo $VALUES | jq -r ".message")
+TRX_ID=${MESSAGE#*ID:}
+echo
+
+echo "GET query bill Transaction by TransactionID"
+echo
+curl -s -X GET http://localhost:4000/channels/mychannel/transactions/$TRX_ID?peer=peer0.org1.example.com \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo "price should be 1.505"
+echo
+echo
+
+
+echo "GET query unit Block by Hash - Hash is $HASH"
+echo
+curl -s -X GET \
+  "http://localhost:4000/channels/mychannel/blocks?hash=$HASH&peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -H "x-access-token: $ORG1_TOKEN"
+echo
+echo
+
+echo "GET query ChainInfo"
+echo
+curl -s -X GET \
+  "http://localhost:4000/channels/mychannel?peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo
+echo
+
+echo "GET query Installed chaincodes"
+echo
+curl -s -X GET \
+  "http://localhost:4000/chaincodes?peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo
+echo
+
+echo "GET query Instantiated chaincodes"
+echo
+curl -s -X GET \
+  "http://localhost:4000/channels/mychannel/chaincodes?peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo
+echo
+
+echo "GET query Channels"
+echo
+curl -s -X GET \
+  "http://localhost:4000/channels?peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo
+echo
+
+echo "Total execution time : $(($(date +%s)-starttime)) secs ..."
+
 echo
 echo " _____   _   _   ____   "
 echo "| ____| | \ | | |  _ \  "
